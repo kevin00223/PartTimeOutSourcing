2e4ce546a7db47d14844fb245b1c71fbd46e3e1e
李凯 <likai.new@gmail.com>
2019-08-20 10:40:06 +0800

    Init project

---
+++ HuangGuan/HuangGuan/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HuangGuan
+//
+//  Created by 李凯 on 2019/8/20.
+//  Copyright © 2019 LK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HuangGuan/HuangGuan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HuangGuan
+//
+//  Created by 李凯 on 2019/8/20.
+//  Copyright © 2019 LK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
